71cd5f52c8b842c5fb15c18ca30aa57d4a7af094
noammirati <noa.ammirati@gmail.com>
2024-01-08 12:46:30 +0100

    Revert "cards for projects page"
    This reverts commit 8e7eed62be16a74aee945914096cf2805e2797e1.

--- app/ui/projects/card.tsx
+++ app/ui/projects/card.tsx
@@ -1,23 +1,17 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from './projects.module.css';
 
 export default function Card({ name, srcPicture, introduction }:
     { name: string, srcPicture: string, introduction: string }) {
     return (
-        <Link href="#" className={`ui-projects-Card ${styles.card}`}>
+        <div className={`ui-projects-Card ${styles.card}`}>
             <Image
                 src={srcPicture}
-                sizes="(min-width: 768px) 80px, 60px"
-                layout='responsive'
-                width={300}
-                height={300}
-                alt={`illustration du projet ${name}`}
-                className={styles.card__img} />
-            <span className={styles.card__footer}>
-                <span>{name}</span>
-                <p>{introduction}</p>
-            </span>
-        </Link>
+                width='200' 
+                height='200'
+                alt={`illustration du projet ${name}`} />
+            <h2>{name}</h2>
+            <p>{introduction}</p>
+        </div>
     )
 }
